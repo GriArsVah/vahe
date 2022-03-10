50238e5be05406c16fa23f0716e97946b69b196c
griarsvah <grigoryanvahe@inbox.ru>
2022-03-10 14:56:03 +0300

    change repository

---
+++ main.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",(async()=>{if("serviceWorker"in navigator)try{const e=await navigator.serviceWorker.register("/sw.js");console.log("Service worker register success",e)}catch(e){console.log("Service worker register fail")}}));
