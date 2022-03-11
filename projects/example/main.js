c68bc5cc01bfacf358bb6d695ddbcf0338f0f290
griarsvah <grigoryanvahe@inbox.ru>
2022-03-11 11:29:54 +0300

    update index.html

--- projects/example/main.js
+++ projects/example/main.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator)
     try {
-      const e = await navigator.serviceWorker.register("/sw.js");
+      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/example/sw.js");
       console.log("Service worker register success example", e);
     } catch (e) {
       console.log("Service worker register fail");
