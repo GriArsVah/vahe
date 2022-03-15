6eed3ed21b8eb51fcd51667dc1ac5e54f46f67d9
griarsvah <grigoryanvahe@inbox.ru>
2022-03-15 14:18:13 +0300

    nav

--- projects/my-programms/js/main.js
+++ projects/my-programms/js/main.js
@@ -1,12 +1,12 @@
-'use strict';
-/* bootstrap nav */
-/* bootstrap nav offcanvas collapse */
-document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function() {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-})
-
-
 (function() {
+'use strict';
+
+    /* bootstrap nav */
+    /* bootstrap nav offcanvas collapse */
+    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function() {
+        document.querySelector('.offcanvas-collapse').classList.toggle('open')
+    })
+
     'use strict'
     document.querySelector('#navbarSideCollapse').addEventListener('click', function() {
         document.querySelector('.offcanvas-collapse').classList.toggle('open')
