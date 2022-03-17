8b635f8e12c5218d2df73c5dae17b1088de0f36b
griarsvah <grigoryanvahe@inbox.ru>
2022-03-17 17:11:22 +0300

    two

--- projects/st47pet/js/main.js
+++ projects/st47pet/js/main.js
@@ -1,6 +1,16 @@
 'use strict';
 /* js code here */
 
+window.addEventListener("load", async () => {
+  if ("serviceWorker" in navigator)
+    try {
+      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/st47pet/sw.js");
+      console.log("Service worker register success example", e);
+    } catch (e) {
+      console.log("Service worker register fail");
+    }
+});
+
 (function () {
     document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
       document.querySelector('.offcanvas-collapse').classList.toggle('open')
---
+++ projects/st47pet/sw.js
@@ -0,0 +1,53 @@
+const staticCacheName = 's-app-v0'
+const dynamicCacheName = 'd-app-v0'
+
+const assetUrls = [
+  'https://vahe.ga/projects/St47Pet/index.html',
+  'https://vahe.ga/projects/St47Pet/js/main.js',
+  'https://vahe.ga/projects/St47Pet/css/main.css',
+  'https://vahe.ga/projects/St47Pet/images/'
+]
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
+})
+
+self.addEventListener('activate', async event => {
+  const cacheNames = await caches.keys()
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
+  )
+})
+
+self.addEventListener('fetch', event => {
+  const {request} = event
+
+  const url = new URL(request.url)
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request))
+  } else {
+    event.respondWith(networkFirst(request))
+  }
+})
+
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (e) {
+    const cached = await cache.match(request)
+    return cached ?? await caches.match('https://vahe.ga/projects/St47Pet/index.html')
+  }
+}
