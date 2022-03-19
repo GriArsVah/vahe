87b1244ed3516165d8bfb7745920973ac77c2109
griarsvah <grigoryanvahe@inbox.ru>
2022-03-19 10:16:40 +0300

    more changed

--- projects/st47pet/js/main.js
+++ projects/st47pet/js/main.js
@@ -4,7 +4,7 @@
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator)
     try {
-      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/st47pet/sw.js");
+      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/st47pet/js/sw.js");
       console.log("Service worker register success example", e);
     } catch (e) {
       console.log("Service worker register fail");
