2a89e8decd29e6f1a070fd7705ae06b2d93e710c
griarsvah <grigoryanvahe@inbox.ru>
2022-04-06 01:55:57 +0300

    add del

--- projects/my-programms/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", async () => {
-  if ("serviceWorker" in navigator)
-    try {
-      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/my-programms/sw.js");
-      console.log("Service worker register success my-programms", e);
-    } catch (e) {
-      console.log("Service worker register fail");
-    }
-});
