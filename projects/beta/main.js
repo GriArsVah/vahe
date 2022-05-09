f12ab7bcb47ea0e3ef9407bceccb2ca818842ddc
Vahe <52396901+GriArsVah@users.noreply.github.com>
2022-05-09 22:35:05 +0300

    del fold proj

--- projects/beta/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", async () => {
-  if ("serviceWorker" in navigator)
-    try {
-      const e = await navigator.serviceWorker.register("https://vahe.ga/projects/example/sw.js");
-      console.log("Service worker register success", e);
-    } catch (e) {
-      console.log("Service worker register fail");
-    }
-});
